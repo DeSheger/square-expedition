26c9740070fba2e727a23235e42c1367fe36a570
groszpiotr <groszpiotr.mitrox@gmail.com>
2024-07-31 20:51:48 +0200

    feat: init window

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Security.Principal;
+using Engine.Core;
+
+using (Window window = new Window(800, 800, "Square Expedition"))
+{
+    window.Run();
+}
