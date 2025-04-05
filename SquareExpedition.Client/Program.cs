12d7dbd783af36aeac796d05b36677882d798ce1
groszpiotr <groszpiotr.mitrox@gmail.com>
2025-04-05 14:16:10 +0200

    Added cross-platform client from Avalonia

--- SquareExpedition.Client/Program.cs
+++ SquareExpedition.Client/Program.cs
@@ -1,7 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-using SquareExpedition.Engine.Core;
+﻿using Avalonia;
+using System;
 
-using (Window window = new Window(800, 800, "Square Expedition"))
+namespace SquareExpedition.Client;
+
+sealed class Program
 {
-    window.Run();
+    // Initialization code. Don't use any Avalonia, third-party APIs or any
+    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
+    // yet and stuff might break.
+    [STAThread]
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
+
+    // Avalonia configuration, don't remove; also used by visual designer.
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+            .UsePlatformDetect()
+            .WithInterFont()
+            .LogToTrace();
 }
---
+++ SquareExpedition.Client/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace SquareExpedition.Client.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
