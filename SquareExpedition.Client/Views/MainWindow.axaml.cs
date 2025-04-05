19e5f6956321704cdb4d4a1b70ac112c03ad3baa
Piotr Grosz <80118253+DeSheger@users.noreply.github.com>
2025-04-05 14:19:07 +0200

    Added cross-platform client from Avalonia (#4)

---
+++ SquareExpedition.Client/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace SquareExpedition.Client.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
