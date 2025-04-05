a92be4072ee5f57b2da06f3402acfd63d88d1b8c
Piotr Grosz <80118253+DeSheger@users.noreply.github.com>
2025-04-05 18:19:02 +0200

    refactor: refactor client from Avalonia to MonoGame framework (#6)

--- SquareExpedition.Application/Core/Window.cs
+++ SquareExpedition.Application/Core/Window.cs
@@ -3,7 +3,7 @@ using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 
-namespace SquareExpedition.Engine.Core;
+namespace SquareExpedition.Application.Core;
 
 public class Window : GameWindow
 {
--- SquareExpedition.Client/Program.cs
+++ SquareExpedition.Client/Program.cs
@@ -1,21 +1,2 @@
-﻿using Avalonia;
-using System;
-
-namespace SquareExpedition.Client;
-
-sealed class Program
-{
-    // Initialization code. Don't use any Avalonia, third-party APIs or any
-    // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
-    // yet and stuff might break.
-    [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
-
-    // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace();
-}
+﻿using var game = new SquareExpedition.Client.Game1();
+game.Run();
